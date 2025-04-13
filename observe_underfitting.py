e0fd7f6b5920f87ec37ed4c9f7cea43eb9104bab
blackhole-hope123 <smile-369@hotmail.com>
2025-04-13 17:56:22 -0400

    updated

---
+++ observe_underfitting.py
@@ -0,0 +1,33 @@
+import tensorflow as tf
+import numpy as np
+from utils import get_model, draw_heatmap, load_train_and_test_data, data_preprocessing
+
+EPOCHS = 10
+    
+def observe_underfitting():
+    # load the train and test data
+    x_train, y_train, x_test, y_test = load_train_and_test_data("data")
+    x_train, y_train, x_test, y_test = np.array(x_train), np.array(y_train), np.array(x_test), np.array(y_test)
+    x_train, x_test = data_preprocessing(x_train, x_test)
+
+    # one hot encoding
+    y_train = tf.keras.utils.to_categorical(y_train)
+    y_test = tf.keras.utils.to_categorical(y_test)
+
+    # a possible range of dropout_rates and regularizer_strengths for underfitting
+    dropout_rates=[0.4,0.6,0.8]
+    regularizer_strengths=[0.4,0.6,0.8]
+    underfitting=[]
+    for dropout_rate in dropout_rates:
+        for regularizer_strength in regularizer_strengths:        
+            model = get_model(dropout_rate,regularizer_strength, batch_normalization=True)
+            
+            model.fit(x_train, y_train, epochs=EPOCHS, batch_size=32)
+            loss,test_accuracy=model.evaluate(x_test, y_test, verbose=2, batch_size=32)
+            underfitting.append([dropout_rate,regularizer_strength,test_accuracy])
+    draw_heatmap(underfitting, False)
+    
+
+if __name__ == "__main__":
+    observe_underfitting()
+
